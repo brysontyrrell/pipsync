7cbae23368d662beb26639b1b887e75f24eab89e
Bryson Tyrrell <bryson.tyrrell@jamf.com>
2020-08-26 15:48:47 -0500

    0.2.0
    Added options for controlling pipsync behavior.

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
-# pipsync
+# Pipsync
 
-pipsync is a utility to help keep requirements.txt files for serverless functions in sync with a parent Pipfile for a project.
+_Pipsync_ is a utility to help keep `requirements.txt` files in sync with a parent `Pipfile` for Serverless Application Model (SAM) projects.
+
+## Installation and Usage
+
+_Pipsync_ will be published on PyPI in the future.
+
+Install _Pipsync_ from GitHub:
+
+```shell script
+% pip install git+git://github.com/brysontyrrell/pipsync
+```
+
+### SAM Project Structure
+
+_Pipsync_ is opinionated on how serverless projects are structured. The directory tree below is an example.
+
+At the root the repository are the `Pipfile` and `Pipfile.lock` which manage the local development environment. Within the project directories for the various Lambda Functions with their respective `requirements.txt` files used by `sam build`.
+
+The locations of the Lambda Functions are not important and these directories can be structured as you wish.
+
+```text
+MyApp/
+├── src/
+│   ├── function_A
+│   │   ├── index.py
+│   │   └── requirements.txt
+│   └── function_B
+│       ├── index.py
+│       └── requirements.txt
+├── Pipfile
+├── Pipfile.lock
+└── template.yaml
+```
+
+### CLI Options
+
+After installing, view _Pipsync's_ options by running `pipsync -h`:
+
+```text                                      
+usage: pipsync [-h] [-x EXCLUDE] [-f] [--dev] [-v] [--version] PATH
+
+Sync requirements.txt files with a project Pipfile.
+
+positional arguments:
+  PATH                  Project root / Pipfile location
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -x EXCLUDE, --exclude EXCLUDE
+                        Exclude top level directories from requirements.txt file search
+  -f, --force           Remove packages in requirements.txt files that are not in the Pipfile
+  --dev                 Include dev-packages from Pipfile.
+  -v, --verbose         Verbose console output.
+  --version             show program's version number and exit
+
+```
+
+_Pipsync_ will scan the `Pipfile` for all of the top-level packages you have installed. It will then scan for `requirements.txt` files within the directory structure. From there, _Pipsync_ will read each file, compare it to the top-level package list, and re-write using the pinned versions in `Pipfile.lock`.
+
+#### Force Sync
+
+By default, _Pipsync_ will ignore packages in `requirements.txt` files that are not listed in your `Pipfile`. These packages will be passed back in without a pinned version. Use the `-f/--force` argument to remove packages in `requirements.txt` that are not found in your `Pipfile`.
+
+#### Dev Packages
+
+By default, _Pipsync_ will not include `dev-packages` from your `Pipfile` when syncing. Use the `--dev` argument to include them.
+
+### Use With Editors and IDEs
+
+PyCharm FileWatcher instructions coming...
--- pipsync/__init__.py
+++ pipsync/__init__.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import logging
 import os
 
 import toml
 
 __title__ = "pipsync"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Bryson Tyrrell"
 __author_email__ = "bryson.tyrrell@gmail.com"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Bryson Tyrrell"
-__description__ = "Sync requirements.txt files with Pipfile.lock"
+__description__ = "Sync requirements.txt files with a project Pipfile."
+
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logger(verbose=False):
+    """Configure the logger. For use when invoked as a CLI tool."""
+    log_level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(level=log_level, format="%(message)s")
 
 
 def arguments():
     parser = argparse.ArgumentParser(prog="pipsync", description=__description__)
-    parser.add_argument("PATH", help="Pipfile.lock location")
+    parser.add_argument("PATH", help="Project root / Pipfile location")
+
     parser.add_argument(
         "-x",
         "--exclude",
@@ -24,13 +35,33 @@ def arguments():
         default=[],
         help="Exclude top level directories from requirements.txt file search",
     )
+
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Remove packages in requirements.txt files that are not in the Pipfile",
+    )
+
+    parser.add_argument(
+        "--dev", action="store_true", help="Include dev-packages from Pipfile."
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Verbose console output."
+    )
+
+    parser.add_argument(
+        "--version", action="version", version="pipsync {}".format(__version__)
+    )
+
     parsed_args = parser.parse_args()
 
     if parsed_args.PATH.endswith("Pipfile.lock"):
         parsed_args.DIR = os.path.dirname(parsed_args.PATH)
     else:
         if not os.path.isfile(os.path.join(parsed_args.PATH, "Pipfile.lock")):
-            print("Pipfile.lock not found at given path")
+            logger.info("Pipfile.lock not found at given path")
             raise SystemExit(1)
         else:
             parsed_args.DIR = parsed_args.PATH
@@ -59,7 +90,7 @@ def find_requirements_files(root_dir: str, exclude: list):
     return file_list
 
 
-def get_pipfile_packages(base_dir):
+def get_pipfile_packages(base_dir, include_dev=False):
     pipfile = toml.load(os.path.join(base_dir, "Pipfile"))
 
     with open(os.path.join(base_dir, "Pipfile.lock"), "r") as f:
@@ -67,17 +98,24 @@ def get_pipfile_packages(base_dir):
 
     values = dict()
 
-    for r in pipfile_lock["default"].keys():
-        if r in pipfile["packages"].keys():
-            # Git installed packages will not have version values
-            version = pipfile_lock["default"][r].get("version")
+    def get_version(package):
+        # Git installed packages will not have version values
+        version = pipfile_lock["default"][package].get("version")
 
-            if not version:
-                git_url = pipfile_lock["default"][r].get("git")
-                if git_url:
-                    values[r] = {"pip": f"git+{git_url}#egg={r}"}
-            else:
-                values[r] = {"pip": f"{r}{version}"}
+        if not version:
+            git_url = pipfile_lock["default"][package].get("git")
+            if git_url:
+                return {"pip": f"git+{git_url}#egg={package}"}
+        else:
+            return {"pip": f"{package}{version}"}
+
+    package_data = dict(pipfile["packages"])
+    if include_dev:
+        package_data.update(pipfile["dev-packages"])
+
+    for p in pipfile_lock["default"].keys():
+        if p in package_data.keys():
+            values[p.lower()] = get_version(p)
 
     return values
 
@@ -95,34 +133,53 @@ def parse_requirements(file_path):
     return requirements
 
 
-def write_requirements(pipfile_packages, requirements_packages, file_path):
-    # TODO: DO NOT REMOVE PACKAGES THAT ARE NOT IN PIPFILE?
-    matched_requirements = set([i.lower() for i in pipfile_packages.keys()]) & set(
-        [i.lower() for i in requirements_packages.keys()]
-    )
+def generate_requirements(pipfile_packages, requirements_packages, force=False):
+    """force: The generated requirements text will remove any packages that are not
+    listed in the pipfile_packages.
+    """
+    content = str()
+    for p in [i.lower() for i in requirements_packages.keys()]:
+        try:
+            content += pipfile_packages[p]["pip"] + "\n"
+        except KeyError:
+            if force:
+                logger.info(f"Force Sync: package '{p}' removed")
+            else:
+                content += p + "\n"
 
-    if matched_requirements:
-        with open(file_path, "w") as f:
-            for package in sorted(matched_requirements):
-                f.write(pipfile_packages[package]["pip"] + "\n")
+    return content
 
-        return matched_requirements
 
-    else:
-        return None
+def write_requirements(content, path):
+    with open(path, "w") as f:
+        f.write(content)
 
 
 def main():
     args = arguments()
-    print(args)
+    configure_logger(args.verbose)
 
     requirements_files = find_requirements_files(args.DIR, args.exclude)
+    if not requirements_files:
+        logger.warning("No requirements.txt files found.")
+        raise SystemExit
 
-    pipfile_packages = get_pipfile_packages(args.DIR)
+    pipfile_packages = get_pipfile_packages(args.DIR, args.dev)
 
+    synced_count = 0
+    skipped_count = 0
     for file in requirements_files:
-        print(file)
-        write_requirements(pipfile_packages, parse_requirements(file), file)
+        if req_content := generate_requirements(
+            pipfile_packages, parse_requirements(file), args.force
+        ):
+            logger.info(f"Syncing file: {file}")
+            write_requirements(req_content, file)
+            synced_count += 1
+        else:
+            logger.debug(f"Empty requirements file: {file}")
+            skipped_count += 1
+
+    logger.info(f"Synced {synced_count} files | Skipped {skipped_count} files")
 
 
 if __name__ == "__main__":
